79d1653e4c6a2da11ecdb841dfab68718df885d4
Django Cass <dj.cass44@gmail.com>
2019-03-20 10:22:10 +1100

    Added exception catching due to network failure

--- src/main/kotlin/dev/castive/fav2/net/JsoupNetworkLoader.kt
+++ src/main/kotlin/dev/castive/fav2/net/JsoupNetworkLoader.kt
@@ -11,16 +11,22 @@ class JsoupNetworkLoader: NetworkLoader {
      * E.g. <link rel="shortcut icon" href="https://github.githubassets.com/favicon.ico">
      */
     override fun getIconPath(domain: String): String? {
-        val document = Jsoup.connect(domain).get()
-        val validIcons = arrayListOf<String>()
-        val icon = document.head().select("link[rel]").select("link[href]")
-        if(Fav.DEBUG) Log.d(javaClass, "Loaded ${icon.size} links")
-        icon.forEach {
-            if(Definitions.contains(it.attr("rel"))) {
-                validIcons.add(it.attr("href"))
-                if(Fav.DEBUG) Log.d(javaClass, it.attr("href"))
+        try {
+            val document = Jsoup.connect(domain).get()
+            val validIcons = arrayListOf<String>()
+            val icon = document.head().select("link[rel]").select("link[href]")
+            if (Fav.DEBUG) Log.d(javaClass, "Loaded ${icon.size} links")
+            icon.forEach {
+                if (Definitions.contains(it.attr("rel"))) {
+                    validIcons.add(it.attr("href"))
+                    if (Fav.DEBUG) Log.d(javaClass, it.attr("href"))
+                }
             }
+            return if (validIcons.isEmpty()) null else validIcons[0]
         }
-        return if(validIcons.isEmpty()) null else validIcons[0]
+        catch (e: Exception) {
+            Log.e(javaClass, "Failed to get icon: $e")
+            return null
+        }
     }
 }
