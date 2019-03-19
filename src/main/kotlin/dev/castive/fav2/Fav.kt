9321dcc7f16286b366f8bdd0d97cd2817feefd57
Django Cass <dj.cass44@gmail.com>
2019-03-20 10:22:39 +1100

    Added policy to block HTTP domains (enabled by default)

--- src/main/kotlin/dev/castive/fav2/Fav.kt
+++ src/main/kotlin/dev/castive/fav2/Fav.kt
@@ -1,12 +1,15 @@
 package dev.castive.fav2
 
+import com.django.log2.logging.Log
 import dev.castive.fav2.net.DirectNetworkLoader
 import dev.castive.fav2.net.JsoupNetworkLoader
 
 object Fav {
     var DEBUG = false
+    var ALLOW_HTTP = false
 
     fun loadDomain(domain: String): String? {
+        if(!checkDomain(domain)) return null
         var icon: String? = DirectNetworkLoader().getIconPath(domain)
         if(icon != null && icon.isNotBlank()) return icon
 
@@ -16,6 +19,10 @@ object Fav {
         return null
     }
     fun loadDomain(domain: String, callback: OnLoadedCallback) {
+        if(!checkDomain(domain)) {
+            callback.onLoad(null)
+            return
+        }
         Thread {
             var icon: String? = DirectNetworkLoader().getIconPath(domain)
             if(icon != null && icon.isNotBlank()) {
@@ -34,6 +41,11 @@ object Fav {
             isDaemon = true
             start()
         }
+    }
+
+    internal fun checkDomain(domain: String): Boolean {
+        if(domain.startsWith("http://") && ALLOW_HTTP && DEBUG) Log.w(javaClass, "Loading of insecure origins is not recommended.")
+        return !domain.startsWith("http://") || ALLOW_HTTP
     }
 
     interface OnLoadedCallback {
--- src/test/kotlin/dev/castive/fav2/FavTest.kt
+++ src/test/kotlin/dev/castive/fav2/FavTest.kt
@@ -1,8 +1,9 @@
 package dev.castive.fav2
 
 import com.django.log2.logging.Log
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -10,6 +11,7 @@ class FavTest {
     @BeforeEach
     internal fun setUp() {
         Fav.DEBUG = true
+        Fav.ALLOW_HTTP = false
     }
 
     @ParameterizedTest
@@ -21,7 +23,26 @@ class FavTest {
     fun getKnown(value: String) {
         val icon = Fav.loadDomain(value)
         Log.d(javaClass, icon)
-        Assertions.assertNotNull(icon)
-        Assertions.assertTrue(icon!!.endsWith("png") || icon.endsWith("ico"))
+        assertNotNull(icon)
+        assertTrue(icon!!.endsWith("png") || icon.endsWith("ico"))
+    }
+
+    @Test
+    fun checkSecure() {
+        Fav.ALLOW_HTTP = false
+        val allowed = Fav.checkDomain("https://google.com")
+        assertTrue(allowed)
+    }
+    @Test
+    fun checkInsecure() {
+        Fav.ALLOW_HTTP = false
+        val allowed = Fav.checkDomain("http://google.com")
+        assertFalse(allowed)
+    }
+    @Test
+    fun checkInsecureAllowed() {
+        Fav.ALLOW_HTTP = true
+        val allowed = Fav.checkDomain("http://google.com")
+        assertTrue(allowed)
     }
 }
