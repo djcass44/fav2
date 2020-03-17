c8c860481dc0cf3475161f826a075ba4f5ddb7c8
Django Cass <django@dcas.dev>
2020-03-17 20:53:32 +1100

    Added support for customisable redis host

--- src/main/kotlin/dev/castive/fav2/config/RedisConfig.kt
+++ src/main/kotlin/dev/castive/fav2/config/RedisConfig.kt
@@ -18,18 +18,25 @@ package dev.castive.fav2.config
 
 
 import dev.castive.fav2.entity.Icon
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 
 @Configuration
 class RedisConfig {
 
+	@Value("\${spring.redis.host:localhost}")
+	lateinit var redisHost: String
+
 	@ConditionalOnMissingBean
 	@Bean
-	fun jedisConnectionFactory() = JedisConnectionFactory()
+	fun jedisConnectionFactory() = JedisConnectionFactory(
+		RedisStandaloneConfiguration(redisHost)
+	)
 
 	@ConditionalOnMissingBean
 	@Bean
